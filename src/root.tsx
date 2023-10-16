c5f28ac4b00bcd42731f40bd1df14caa200438ef
kynhix <kristopher.t.hicks@gmail.com>
2023-10-15 21:09:59 -0400

    Initial commit
    Added solid-start, bootstrap, supabase

---
+++ src/root.tsx
@@ -0,0 +1,44 @@
+// @refresh reload
+import { Navbar } from "solid-bootstrap";
+import { Suspense } from "solid-js";
+import {
+  A,
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title,
+} from "solid-start";
+import Header from "./components/Header";
+import "./root.css";
+
+export default function Root() {
+  return (
+    <Html lang="en">
+      <Head>
+        <Title>SolidStart - Bare</Title>
+        <Meta charset="utf-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous" />
+      </Head>
+      <Body>
+        <Suspense>
+          <ErrorBoundary>
+            <Header />
+            <Routes>
+              <FileRoutes />
+            </Routes>
+          </ErrorBoundary>
+        </Suspense>
+        <Scripts />
+      </Body>
+    </Html>
+  );
+}
