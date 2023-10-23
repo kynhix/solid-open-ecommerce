e6f2d4d3814c8902e86ab5b6be4dc8558a982779
kynhix <kristopher.t.hicks@gmail.com>
2023-10-23 18:50:38 -0400

    Updated side cart items to a base test

--- src/components/SideCartItem.tsx
+++ src/components/SideCartItem.tsx
@@ -1,13 +1,14 @@
-import { Stack } from "solid-bootstrap";
 import { VoidProps } from "solid-js";
 import { shopItem } from "~/shop";
 
 export default function SideCartItem({ product }: VoidProps<{ product: shopItem }>) {
   return (
-    <div>
-      <div class="bg-light border">{product.name}</div>
-      <div class="bg-light border">Second item</div>
-      <div class="bg-light border">Third item</div>
+    <div class="tw-flex tw-flex-row tw-h-28 tw-w-full tw-overflow-hidden">
+      <img src={product.img} class="tw-aspect-square" />
+      <div class="tw-flex-col">
+        <div class="bg-light border"><p class="tw-m-0">{product.name}</p></div>
+        <div class="bg-light border">This is a really long string to test the bullshit and stuff</div>
+      </div>
     </div>
   )
 }
